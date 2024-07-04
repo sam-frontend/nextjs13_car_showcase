c166d1ba32f02c96e86ac794952153cf67c1223c
Sam ugonna <sam_ug@yahoo.com>
2024-07-04 02:36:57 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import "./globals.css";
 import { Footer, NavBar } from "@components";
 
 export const metadata = {
-  title: "Car Hub",
-  description: "Discover world's best car showcase application",
+  title: "Sam-Car Deals",
+  description: "Check out  best car deals",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
